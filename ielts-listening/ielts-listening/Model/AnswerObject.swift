80808f1bbc0401da0219945f2e0f30a6702a3b55
Binh Le <mlequocbinh@gmail.com>
2016-05-13 15:13:32 +0800

    add models

---
+++ ielts-listening/ielts-listening/Model/AnswerObject.swift
@@ -0,0 +1,9 @@
+//
+//  AnswerObject.swift
+//  ielts-listening
+//
+//  Created by Binh Le on 5/13/16.
+//  Copyright © 2016 Binh Le. All rights reserved.
+//
+
+import Foundation
---
+++ ielts-listening/ielts-listening/Model/LessonObject.swift
@@ -0,0 +1,9 @@
+//
+//  LessonObject.swift
+//  ielts-listening
+//
+//  Created by Binh Le on 5/13/16.
+//  Copyright © 2016 Binh Le. All rights reserved.
+//
+
+import Foundation
