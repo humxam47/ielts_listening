4387f77f033b852fcb410ceca88b7fa281ec4ae6
Binh Le <mlequocbinh@gmail.com>
2016-05-16 15:55:16 +0800

    detail-controller-play audio

--- ielts-listening/ielts-listening/ControllerView.swift
+++ ielts-listening/ielts-listening/ControllerView.swift
@@ -15,12 +15,13 @@ class ControllerView: UIView, AVAudioPlayerDelegate {
     @IBOutlet weak var playButton:UIButton!
     @IBOutlet weak var exerciseButton:UIButton!
     
-    var audioPlayer:AVAudioPlayer!
-    
     var lessonArray:NSMutableArray!
     var selectedIndex:Int = 0
     
     weak var controllerDelegate:ControllerDelegate!
+    
+    var audioPlayer:AVAudioPlayer!
+    var progressTimer:NSTimer!
     
     func initController(lessonArray:NSMutableArray, selectedIndex:Int) {
         self.lessonArray = lessonArray
@@ -36,15 +37,46 @@ class ControllerView: UIView, AVAudioPlayerDelegate {
             let soundData = NSData(contentsOfURL:NSURL(string:soundURL)!)
             self.audioPlayer = try AVAudioPlayer(data: soundData!)
             audioPlayer.prepareToPlay()
-            audioPlayer.volume = 1.0
+            audioPlayer.volume = AVAudioSession.sharedInstance().outputVolume
             audioPlayer.delegate = self
             audioPlayer.play()
+            if self.audioPlayer != nil {
+                self.initProgress()
+            }
         } catch {
             print("Error getting the audio file")
         }
     }
     
+    func initProgress() {
+        dispatch_async(dispatch_get_main_queue()) {
+            self.slider.minimumValue = 0.0
+            self.slider.maximumValue = Float(self.audioPlayer.duration)
+            self.progressTimer = NSTimer.scheduledTimerWithTimeInterval(0.0, target: self, selector: #selector(self.updateProgress), userInfo: nil, repeats: true)
+            NSRunLoop.mainRunLoop().addTimer(self.progressTimer, forMode: NSRunLoopCommonModes)
+        }
+    }
+    
+    func updateProgress() {
+        self.slider.value = Float(self.audioPlayer.currentTime)
+        self.timeLabel.text = self.formatTime(Int(self.audioPlayer.duration - self.audioPlayer.currentTime))
+    }
+    
+    func formatTime(totalSecond:Int) -> String {
+        let twoDecimalSecond = String(format: "%02d", totalSecond % 60)
+        let twoDecimalMinutes = String(format: "%02d", (totalSecond / 60) % 60)
+        return "-\(twoDecimalMinutes):\(twoDecimalSecond)"
+    }
+    
     @IBAction func playAction(sender:UIButton) {
+        sender.tag *= -1
+        if sender.tag > 0 {
+            self.playButton.setBackgroundImage(UIImage(named: "icon_pause.png"), forState:UIControlState.Normal)
+            self.audioPlayer.play()
+        } else {
+            self.playButton.setBackgroundImage(UIImage(named: "icon_play.png"), forState:UIControlState.Normal)
+            self.audioPlayer.pause()
+        }
     }
     
     @IBAction func nextAction(sender:UIButton) {
@@ -62,6 +94,8 @@ class ControllerView: UIView, AVAudioPlayerDelegate {
     }
     
     @IBAction func sliderChanged(sender:UISlider) {
+        self.audioPlayer.currentTime = NSTimeInterval(self.slider.value)
+        self.timeLabel.text = self.formatTime(Int(self.audioPlayer.duration - NSTimeInterval(self.slider.value)))
     }
     
     @IBAction func exerciseAction(sender:UIButton) {
@@ -73,6 +107,12 @@ class ControllerView: UIView, AVAudioPlayerDelegate {
             self.exerciseButton.setTitle("Transcript", forState: UIControlState.Normal)
             self.controllerDelegate?.showExerciseView()
         }
+    }
+    
+    func audioPlayerDidFinishPlaying(player: AVAudioPlayer, successfully flag: Bool) {
+        self.slider.value = 0.0
+        self.progressTimer.invalidate()
+        self.progressTimer = nil
     }
     
 }
